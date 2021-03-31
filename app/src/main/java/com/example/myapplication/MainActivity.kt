a149b6d81af06d8a18e9f67e1b605e30e8b71a24
PhilB93 <77934493+PhilB93@users.noreply.github.com>
2021-03-31 10:59:09 +0300

    1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,38 +1,61 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.view.View
+import android.widget.*
 import com.example.myapplication.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.xml.datatype.DatatypeConstants.MONTHS
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("ResourceAsColor")
+   private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+      binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val car1 = Car("car1", 2003)
-        val car2 = Car("car22", 2103)
-        val car3 = Car("car3", 2005)
-        val car4 = Car("car4", 2007)
-        val car5 = Car("car5", 2006)
-        val list = mutableListOf<Car>()
-        list.add(car1)
-        list.add(car2)
-        list.add(car3)
-        list.add(car4)
-        list.add(car5)
-
-        val listView = binding.listView
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
-        listView.adapter = arrayAdapter
-      listView.setOnItemClickListener { parent, view, position, id -> val intent = Intent(this, SecondActivity::class.java)
-          startActivity(intent) }
+    binding.btnAge.setOnClickListener {
+        ageChooser(it)
+        Toast.makeText(this, "Hi!", Toast.LENGTH_SHORT).show() }
     }
+
+    //Календарь
+    @SuppressLint("SetTextI18n")
+    private fun ageChooser(view: View) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+
+          val dpd =  DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, sYear, sMonth, sDayOfMonth ->
+            val selectedDate = "$sDayOfMonth.${sMonth+1}.$sYear"
+
+                binding.textDate.text = selectedDate
+                val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.US)
+                val theDate = sdf.parse(selectedDate)
+                val dateInDays = theDate!!.time/86400000
+                val currentTime = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val curDay = currentTime!!.time/86400000
+                val difference = curDay  - dateInDays
+                binding.textAgeInMin.text = difference.toString()
+            }
+                , year
+                , month
+                , day)
+
+        dpd.datePicker.maxDate = Date().time
+        dpd.show()
+
+        }
+
+
+
 }
 
 
