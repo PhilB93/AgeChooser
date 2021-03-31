453c99bb754dd42adb7f431b2933062e210849c4
PhilB93 <77934493+PhilB93@users.noreply.github.com>
2021-03-31 11:02:34 +0300

    1

--- app/src/main/java/com/example/myapplication/Car.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication
-
-class Car(private val name:String, private val year:Int) {
-
-    override fun toString(): String {
-        return "Car(name='$name', year=$year)"
-    }
-}
